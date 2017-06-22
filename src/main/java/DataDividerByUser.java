0b38a53b34321cd9c8c4cbbcf7780b7e5c1aea8a
Tim Huang <thuang@edmunds.com>
2017-06-21 20:07:35 -0700

    source files and maven files

---
+++ src/main/java/DataDividerByUser.java
@@ -0,0 +1,64 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+import java.io.IOException;
+
+public class DataDividerByUser {
+	public static class DataDividerMapper extends Mapper<LongWritable, Text, IntWritable, Text> {
+
+		// map method
+		@Override
+		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+
+			//input user,movie,rating
+			//divide data by user
+            String[] line = value.toString().trim().split(",");
+            context.write(new IntWritable(Integer.parseInt(line[0])), new Text(line[1] + ":" + line[2]));
+		}
+	}
+
+	public static class DataDividerReducer extends Reducer<IntWritable, Text, IntWritable, Text> {
+		// reduce method
+		@Override
+		public void reduce(IntWritable key, Iterable<Text> values, Context context)
+				throws IOException, InterruptedException {
+            StringBuilder sb = new StringBuilder();
+			//merge data for one user
+            for (Text t : values) {
+                sb.append(t.toString());
+                sb.append(",");
+            }
+            context.write(key, new Text(sb.toString()));
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+
+		Configuration conf = new Configuration();
+
+		Job job = Job.getInstance(conf);
+		job.setMapperClass(DataDividerMapper.class);
+		job.setReducerClass(DataDividerReducer.class);
+
+		job.setJarByClass(DataDividerByUser.class);
+
+		job.setInputFormatClass(TextInputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
+		job.setOutputKeyClass(IntWritable.class);
+		job.setOutputValueClass(Text.class);
+
+		TextInputFormat.setInputPaths(job, new Path(args[0]));
+		TextOutputFormat.setOutputPath(job, new Path(args[1]));
+
+		job.waitForCompletion(true);
+	}
+
+}
